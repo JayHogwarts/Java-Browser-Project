ec65fbaf1220208b3c92cc587b9d025925134ba7
JayHogwarts <jaysaulhowarth@gmail.com>
2017-04-03 19:44:57 +0100

    Created the JEditorPane

--- src/mainProgram/BrowserWindow.java
+++ src/mainProgram/BrowserWindow.java
@@ -1,9 +1,11 @@
 package mainProgram;
 
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 
 public class BrowserWindow {
 	private static JFrame frame = new JFrame("Web Browser");
+	JEditorPane jep = new JEditorPane();
 	
 	public BrowserWindow(){
 		generateWindow();
@@ -12,6 +14,9 @@ public class BrowserWindow {
 	
 	private void generateWindow(){
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jep.setSize(400,400);
+		frame.add(jep);
+		frame.pack();
 		frame.setVisible(true);
 	}
 }
