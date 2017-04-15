d7bc4c410ca59f606fcea3aad907dbd55ab6b16d
JayHogwarts <jaysaulhowarth@gmail.com>
2017-04-15 01:15:44 +0100

    Added most of the gui components, need listeners next

--- src/mainProgram/BrowserWindow.java
+++ src/mainProgram/BrowserWindow.java
@@ -1,39 +1,56 @@
 package mainProgram;
 
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class BrowserWindow {
-	 static JFrame frame = new JFrame("Web Browser");
-	 JEditorPane jep = new JEditorPane();
+	JFrame frame = new JFrame("Web Browser");
+	JEditorPane jep = new JEditorPane();
+	JPanel toolbar = new JPanel();
+	JTextField addressbar = new JTextField(30);
+	JButton homeBut = new JButton("Home");
+	JButton historyBut = new JButton("History");
+	JButton bookmarkBut = new JButton("Bookmarks");
+
 	String url = "https://www.google.co.uk/";
+	String home = "https://www.google.co.uk/";
+	GridBagConstraints gbc = new GridBagConstraints();
 
 	public BrowserWindow() {
 		generateWindow();
+		generateToolBar();
+		generateEditorPane();
+		goHome();
+		frame.setVisible(true);
 	}
 
 	private void generateWindow() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new FlowLayout());
-		frame.pack();
-		frame.setVisible(true);
+		frame.setLayout(new GridBagLayout());
+		frame.setSize(new Dimension(900, 600));
 	}
-	
+
 	private void generateEditorPane() {
 		JScrollPane editorScrollPane = new JScrollPane(jep);
 		editorScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		editorScrollPane.setPreferredSize(new Dimension(500, 500));
-		editorScrollPane.setMinimumSize(new Dimension(100, 100));
-		jep.setPreferredSize(new Dimension(500, 500));
 		jep.setEditorKit(JEditorPane.createEditorKitForContentType("text/html"));
 		jep.setEditable(false);
-		frame.add(editorScrollPane);
+
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.gridy = 1;
+
+		frame.add(editorScrollPane, gbc);
 	}
 
 	public void setURL(String url) {
@@ -44,6 +61,15 @@ public class BrowserWindow {
 		return this.url;
 	}
 
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	// Reloads the current URL
 	public void refresh() {
 		try {
 			jep.setPage(url);
@@ -51,5 +77,59 @@ public class BrowserWindow {
 			System.err.println("URL error with the following URL:" + url);
 		}
 	}
+
+	// Returns the user to their home URL
+	public void goHome() {
+		try {
+			jep.setPage(home);
+		} catch (IOException e) {
+			System.err.println("URL error with the following URL:" + url);
+		}
+	}
 	
+	private void generateAddressBar(){
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.gridx = 1;
+		
+		toolbar.add(addressbar,gbc);
+	}
+	
+	private void generateHomeButton(){
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.gridx = 0;
+		toolbar.add(homeBut);
+	}
+	
+	private void generateHistoryButton(){
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.gridx = 3;
+		toolbar.add(historyBut);
+	}
+	
+	private void generateBookmarkButton(){
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.gridx = 4;
+		toolbar.add(bookmarkBut);
+	}
+	private void generateToolBar(){
+		toolbar.setLayout(new GridBagLayout());
+		generateHomeButton();
+		generateAddressBar();
+		generateHistoryButton();
+		generateBookmarkButton();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 0.03;
+		gbc.weighty = 0.03;
+		gbc.gridy = 0;
+		frame.add(toolbar,gbc);
+	}
+
 }
